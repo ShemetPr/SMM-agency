f53c82b377d259028fc8a783fb510d32dbdf38de
AnastasiyaShemet <Anastasiya>
2022-06-21 14:02:10 +0300

    feat: add css and js files

---
+++ js/hide-show-button.js
@@ -0,0 +1,25 @@
+function myFunction() {
+    var x = document.getElementById("hide_block");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  function myFunction1() {
+    var x = document.getElementById("hide_block_1");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+  function myFunction2() {
+    var x = document.getElementById("hide_block_2");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
